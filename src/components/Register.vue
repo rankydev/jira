734b4212c06675122ac9fa9e4bb8b93d79ce3f66
check files after linting <ranko.spegar@towa.at>
2020-03-22 17:51:28 +0100

    created register component

---
+++ src/components/Register.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-content>
+    <v-container class="fill-height" fluid>
+      <v-row align="center" justify="center">
+        <v-col cols="12" lg="10">
+          <v-form>
+            <v-text-field type="text" label="Name" required></v-text-field>
+            <v-text-field type="text" label="Nachname" required></v-text-field>
+            <v-text-field type="email" label="E-mail" required></v-text-field>
+            <v-text-field
+              type="password"
+              label="Password"
+              required
+            ></v-text-field>
+            <v-text-field
+              type="password"
+              label="Confirm Password"
+              required
+            ></v-text-field>
+            <v-btn x-large block class="center primary mt-4" @click="submit"
+              >Register</v-btn
+            >
+          </v-form>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-content>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: "",
+      lastname: "",
+      email: "",
+      password: "",
+      password2: ""
+    };
+  },
+  methods: {
+    submit() {
+      console.log("hello");
+    }
+  }
+};
+</script>
+
+<style lang="scss"></style>
