ff02ffa562fcd1b50aedbe5f3e78b34b046e8e80
check files after linting <ranko.spegar@towa.at>
2020-03-22 04:23:08 +0100

    created login route and component

---
+++ src/components/Login.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-content>
+    <v-container class="fill-height" fluid>
+      <v-row align="center" justify="center">
+        <v-col cols="12" lg="10">
+          <v-form>
+            <v-text-field :type="email" label="E-mail" required></v-text-field>
+            <v-text-field
+              :type="password"
+              label="Password"
+              required
+            ></v-text-field>
+            <v-btn class="center" @click="submit">Login</v-btn>
+          </v-form>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-content>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss"></style>
---
+++ src/views/Login.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-content>
+    <v-container class="fill-height" fluid>
+      <v-row align="center" justify="center">
+        <v-col cols="12" sm="8" md="4">
+          <v-card class="elevation-12">
+            <v-toolbar color="primary" dark flat>
+              <v-toolbar-title>Login</v-toolbar-title>
+              <v-spacer />
+            </v-toolbar>
+            <v-card-text>
+              <v-form class="pa-4 input-fields">
+                <v-text-field label="Login" name="login" type="text" />
+                <v-text-field
+                  id="password"
+                  label="Password"
+                  name="password"
+                  type="password"
+                />
+              </v-form>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer />
+              <v-btn color="primary">Login</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-content>
+</template>
+
+<script>
+export default {
+  props: {
+    source: String
+  }
+};
+</script>
+
+<style lang="scss"></style>
