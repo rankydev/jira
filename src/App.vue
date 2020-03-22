c7282a4d66f5131c504e101fceeb5e545023fec3
check files after linting <ranko.spegar@towa.at>
2020-03-22 23:03:06 +0100

    added loading spinner

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,51 @@
 <template>
   <v-app class="blue lighten-5">
     <router-view></router-view>
+
+    <div class="spinner" v-if="showSpinner">
+      <div class="spinner-inner">
+        <v-progress-circular
+          size="100"
+          indeterminate
+          color="primary"
+        ></v-progress-circular>
+      </div>
+    </div>
   </v-app>
 </template>
 
-<style lang="scss"></style>
+<script>
+import EventBus from "./eventBus";
+
+export default {
+  data() {
+    return {
+      showSpinner: false
+    };
+  },
+  mounted() {
+    EventBus.$on("CALL_IN_PROGRESS", payload => {
+      this.showSpinner = payload;
+    });
+  }
+};
+</script>
+
+<style lang="scss">
+.spinner {
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  background: #fff;
+  opacity: 0.95;
+}
+
+.spinner-inner {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+</style>
