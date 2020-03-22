8f296ecea9bcbfa61dd16e035fb5a8294c7bc6ca
check files after linting <ranko.spegar@towa.at>
2020-03-22 23:04:24 +0100

    added vuex functions and initialized firebase

--- src/main.js
+++ src/main.js
@@ -1,14 +1,27 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import vuetify from './plugins/vuetify';
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import vuetify from "./plugins/vuetify";
+import * as firebase from "firebase";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
   vuetify,
-  render: h => h(App)
-}).$mount('#app')
+  render: h => h(App),
+  created() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyBrgtVUnKa6v9eLLulX3KiuCgEqs5y-jm8",
+      authDomain: "jira-dashboard-43191.firebaseapp.com",
+      databaseURL: "https://jira-dashboard-43191.firebaseio.com",
+      projectId: "jira-dashboard-43191",
+      storageBucket: "jira-dashboard-43191.appspot.com",
+      messagingSenderId: "1091749799228",
+      appId: "1:1091749799228:web:e02e79e264adf8cc6eaa39",
+      measurementId: "G-T8T2XVVKRY"
+    });
+  }
+}).$mount("#app");
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,65 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
+import * as firebase from "firebase";
+import EventBus from "../eventBus";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    user: null,
+    error: null
   },
   mutations: {
+    setUser(state, payload) {
+      state.user = payload;
+    },
+    setError(state, payload) {
+      return (state.error = payload);
+    },
+    clearError(state) {
+      return (state.error = null);
+    }
   },
   actions: {
+    registerUser({ commit }, payload) {
+      EventBus.$emit("CALL_IN_PROGRESS", true);
+      commit("clearError");
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(payload.email, payload.password)
+        .then(user => {
+          commit("setUser", user);
+          EventBus.$emit("CALL_IN_PROGRESS", false);
+        })
+        .catch(err => {
+          commit("setError", err);
+          EventBus.$emit("CALL_IN_PROGRESS", false);
+        });
+    },
+    loginUser({ commit }, payload) {
+      EventBus.$emit("CALL_IN_PROGRESS", true);
+      commit("clearError");
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(payload.email, payload.password)
+        .then(user => {
+          commit("setUser", user);
+          EventBus.$emit("CALL_IN_PROGRESS", false);
+        })
+        .catch(err => {
+          commit("setError", err);
+          EventBus.$emit("CALL_IN_PROGRESS", false);
+        });
+    }
   },
-  modules: {
-  }
-})
+  getters: {
+    getUser(state) {
+      return state.user;
+    },
+    getError(state) {
+      return state.error;
+    }
+  },
+  modules: {}
+});
